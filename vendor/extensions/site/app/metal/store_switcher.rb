d33ca42dbca0ee1d1460ea1239d1891de251ba45
Steven Jenkins <steven@endpoint.com>
2009-11-27 12:03:04 -0500

    minor whitespace cleanup -- feel free to squash

--- vendor/extensions/site/app/metal/store_switcher.rb
+++ vendor/extensions/site/app/metal/store_switcher.rb
@@ -6,20 +6,20 @@ class StoreSwitcher
     session = env['rack.session']
     domain_parts = env['SERVER_NAME'].split(".")
     domain_name = domain_parts.size == 3 ? domain_parts[1] : domain_parts[0]
-    
+
     remove_path = nil
 
     if env['PATH_INFO'] =~ /^\/people/
-      remove_path = "people" 
+      remove_path = "people"
       session[:store] = "nwb"
       session[:ignore_domain] = true
-      
+
     elsif env['PATH_INFO'] =~ /^\/pets/
-      remove_path = "pets" 
+      remove_path = "pets"
       session[:store] = "pwb"
       session[:ignore_domain] = true
-      
-    elsif !session[:ignore_domain]
+
+	elsif !session[:ignore_domain]
       session[:store] = case domain_name
         when "petwellbeing" then "pwb"
         when "naturalwellbeing" then "nwb"
@@ -29,14 +29,14 @@ class StoreSwitcher
     end
 
     if session[:store] == "pwb"
-      Spree::Config.set(:site_name => "PetWellBeing.com") 
+      Spree::Config.set(:site_name => "petwellbeing.com")
     else
-      Spree::Config.set(:site_name => "NaturalWellBeing.com") 
+      Spree::Config.set(:site_name => "naturalwellbeing.com")
     end
-    
+
     session[:started_from] = session[:store] unless session.key? :started_from
- 
-    unless remove_path.nil?
+
+	unless remove_path.nil?
       new_path = env['PATH_INFO'].gsub "/#{remove_path}", ""
       env["REQUEST_PATH"] = new_path
       env["REQUEST_URI"]  = new_path
