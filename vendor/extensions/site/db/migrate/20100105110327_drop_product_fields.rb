896b464adf5b5dabb4a43c185ceb8fdbc5d99985
Brian Quinn <briandquinn@gmail.com>
2010-01-05 13:49:50 +0000

    Removed all export fields from products

--- vendor/extensions/site/db/migrate/20100105110327_drop_product_fields.rb
+++ vendor/extensions/site/db/migrate/20100105110327_drop_product_fields.rb
@@ -7,9 +7,17 @@ class DropProductFields < ActiveRecord::Migration
     remove_column :products, :short_category
     remove_column :products, :short_subcategory
     remove_column :products, :short_brand
+    remove_column :products, :export_description_broad
+    remove_column :products, :export_description_specific
+    remove_column :products, :country_id
+    remove_column :products, :tarrif_code
   end
 
   def self.down
+    add_column :products, :tarrif_code, :string
+    add_column :products, :country_id, :integer
+    add_column :products, :export_description_specific, :string
+    add_column :products, :export_description_broad, :string
     add_column :products, :short_brand, :text
     add_column :products, :short_subcategory, :text
     add_column :products, :short_category, :text
