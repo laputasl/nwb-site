0119768b8dd66c176cb718d4f2b5a1c1a7eb0609
Brian Quinn <briandquinn@gmail.com>
2010-01-05 10:49:47 +0000

    Add rbet plugin for ExactTarget integration

--- config/environment.rb
+++ config/environment.rb
@@ -45,8 +45,6 @@ Spree::Initializer.run do |config|
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
   config.plugins = [ :all, :resource_controller, :extension_patches ]
 
-  config.extensions = [:theme_default, :all]
-
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
