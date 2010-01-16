9bc9c16cda56dfb5830fdebe7f48674e6b0b0cb6
Brian Quinn <briandquinn@gmail.com>
2010-01-16 12:54:45 +0000

    Updated to latest Spree edge

--- vendor/extensions/multi_store/multi_store_extension.rb
+++ vendor/extensions/multi_store/multi_store_extension.rb
@@ -1,12 +1,12 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application'
-                             
-# ------------------------------------------------------------------------------------  
-# NOTE: This is a placeholder for the future multi store extension.  I needed to add 
-# some functionality to the GA that was particular to multi store and didn't feel 
+
+# ------------------------------------------------------------------------------------
+# NOTE: This is a placeholder for the future multi store extension.  I needed to add
+# some functionality to the GA that was particular to multi store and didn't feel
 # like jamming it into site extension only to tease it out again.  We'll put the rest
 # of the multi store stuff in here eventually.
-# ------------------------------------------------------------------------------------  
+# ------------------------------------------------------------------------------------
 
 class MultiStoreExtension < Spree::Extension
   version "1.0"
@@ -18,26 +18,26 @@ class MultiStoreExtension < Spree::Extension
   # def self.require_gems(config)
   #   config.gem "gemname-goes-here", :version => '1.2.3'
   # end
-  
-  def activate       
-    
+
+  def activate
+
     # sample custom goal
-    # OrdersController.class_eval do 
+    # OrdersController.class_eval do
     #   create.before << :fire_goal
-    #   
+    #
     #   private
-    #   def fire_goal   
+    #   def fire_goal
     #     flash[:analytics] = "/goal/add-to-cart"
     #   end
-    # end    
-    
+    # end
+
     Tracker.class_eval do
       belongs_to :store
-      
+
       def self.current
         trackers = Tracker.find(:all, :conditions => {:active => true, :environment => ENV['RAILS_ENV']})
 debugger
-        trackers.select { |t| t.store.name == Spree::Config[:site_name] }.first      
+        trackers.select { |t| t.store.name == Spree::Config[:site_name] }.first
       end
     end
 
