361955cd27289332ba0d4c6ef9efa7f33d607875
Brian Quinn <briandquinn@gmail.com>
2010-01-18 16:41:39 +0000

    Added ActiveShipping with sample data for calculators

---
+++ vendor/extensions/site/db/sample/zone_members.rb
@@ -0,0 +1,3 @@
+canada_zone = ZoneMember.find_by_zoneable_id(35)
+canada_zone.zone_id = 3
+canada_zone.save!
--- vendor/extensions/site/db/seeds.rb
+++ vendor/extensions/site/db/seeds.rb
@@ -4,4 +4,12 @@ Spree::Config.set(:allow_ssl_in_production => false)
 Spree::Config.set(:exact_target_user => "NWB_API_USER")
 Spree::Config.set(:exact_target_password => "vki1234!")
 
-#Spree::Config.set("searcher.spelling_suggestion" => true)
+#Spree::Config.set("searcher.spelling_suggestion"  => true)
+
+Spree::ActiveShipping::Config.set(:origin_country => 'US')
+Spree::ActiveShipping::Config.set(:origin_city => 'Atlanta')
+Spree::ActiveShipping::Config.set(:origin_state => 'GA')
+Spree::ActiveShipping::Config.set(:origin_zip => '30354')
+Spree::ActiveShipping::Config.set(:ups_login => 'naturalwellbeing')
+Spree::ActiveShipping::Config.set(:ups_password => 'th33mp1r3')
+Spree::ActiveShipping::Config.set(:ups_key => 'ABA12A047F52CA84')
