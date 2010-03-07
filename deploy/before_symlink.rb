cdaad8e55ff00360e458d3e87d49f97df2abc384
Sean Schofield <sean@railsdog.com>
2010-03-06 21:08:44 -0500

    Create symlinks for xapiandb as well.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,1 +1,2 @@
-run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml" 
+run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml"      
+run "ln -nfs #{shared_path}/db/xapiandb #{release_path}/db/xapiandb"
