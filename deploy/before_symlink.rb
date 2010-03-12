bd417e34ae2253c57597b0c606976e6d239dc2f8
Sean Schofield <sean@railsdog.com>
2010-03-12 10:58:13 -0500

    Create symlinks for static_people and static_pets.  Also symlink the public directories to work in multi site.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,10 @@
 run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml"      
 run "ln -nfs #{shared_path}/db/xapiandb #{release_path}/db/xapiandb"
+run "ln -nfs #{shared_path}/static_people #{release_path}/public/sl"
+run "ln -nfs #{shared_path}/static_pets #{release_path}/public/st"
+
+%w(pets people).each do |store|
+  origin_path= File.join(release_path, 'public')
+  destination_path = File.join(release_path, 'public', store)
+  run "ln -nsf #{origin_path} #{destination_path}"
+end
