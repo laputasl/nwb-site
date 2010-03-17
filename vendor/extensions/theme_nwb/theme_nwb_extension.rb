8a3bc48dcfd6ca8ac6d59bff24e4c4f101428dc7
Brian Quinn <briandquinn@gmail.com>
2010-03-17 14:00:53 +0000

    Added alt text for images

--- vendor/extensions/theme_nwb/theme_nwb_extension.rb
+++ vendor/extensions/theme_nwb/theme_nwb_extension.rb
@@ -11,13 +11,13 @@ class ThemeNwbExtension < Spree::Extension
   # def self.require_gems(config)
   #   config.gem "gemname-goes-here", :version => '1.2.3'
   # end
-  
+
   def activate
     # make your helper avaliable in all views
     Spree::BaseController.class_eval do
       helper NwbThemeHelper
     end
-    
+
     Spree::BaseHelper.module_eval do
 
       def global_categories
@@ -25,31 +25,6 @@ class ThemeNwbExtension < Spree::Extension
         Taxonomy.find(:first, :conditions => {:store_id => current_site.id, :name => "Category"})
       end
 
-
-      def mini_image(product, options={})
-        if product.images.empty?
-          image_tag "/#{@site.code}/images/noimage/mini.png", options
-        else
-          image_tag product.images.first.attachment.url(:mini), options
-        end
-      end
-
-      def small_image(product, options={})
-        if product.images.empty?
-          image_tag "/#{@site.code}/images/noimage/small.png", options
-        else
-          image_tag product.images.first.attachment.url(:small), options
-        end
-      end
-
-      def product_image(product, options={})
-        if product.images.empty?
-          image_tag "/#{@site.code}/images/noimage/product.png", options
-        else
-          image_tag product.images.first.attachment.url(:product), options
-        end
-      end
-      
     end
   end
 end
