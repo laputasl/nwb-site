a20c99f88b54639a2533339bd42ada5a11ea76c7
Brian Quinn <briandquinn@gmail.com>
2010-03-24 08:41:04 +0000

    Ensure correct partials are displayed when browsing /pets & /people

--- vendor/extensions/site/app/controllers/home_page_controller.rb
+++ vendor/extensions/site/app/controllers/home_page_controller.rb
@@ -5,12 +5,12 @@ class HomePageController < Spree::BaseController
 
     #TODO: Figure out what criteria we will use to display products on homepages.
     if @site.code == "pwb"
-      @best_selling_cats = Taxon.find_by_name("Cat Supplies").products.active[0...2]
-      @best_selling_dogs = Taxon.find_by_name("Dog Supplies").products.active[0...2]
+      @best_selling_cats = Taxon.find_by_name("Cat Supplies").products.active.find(:all, :limit => 2)
+      @best_selling_dogs = Taxon.find_by_name("Dog Supplies").products.active.find(:all, :limit => 2)
     else
-      @best_selling_products = Product.active.find_all_by_store_id(@site.id)[0...10]
+      @best_selling_products = Product.active.find_all_by_store_id(@site.id, :limit => 10)
     end
 
-    render :partial => "#{@site.code}_show", :layout => true
+    render :partial => "#{@current_domain}_show", :layout => true
   end
 end
