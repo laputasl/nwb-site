8bc91a866ceb549bb39356ae467bdd01a8ee1958
Brian Quinn <briandquinn@gmail.com>
2010-04-26 09:34:01 +0100

    Update UPS API key

--- vendor/extensions/site/db/seeds.rb
+++ vendor/extensions/site/db/seeds.rb
@@ -12,7 +12,7 @@ Spree::ActiveShipping::Config.set(:origin_state => 'GA')
 Spree::ActiveShipping::Config.set(:origin_zip => '30354')
 Spree::ActiveShipping::Config.set(:ups_login => 'naturalwellbeing')
 Spree::ActiveShipping::Config.set(:ups_password => 'th33mp1r3')
-Spree::ActiveShipping::Config.set(:ups_key => 'ABA12A047F52CA84')
+Spree::ActiveShipping::Config.set(:ups_key => '3B9D6481C487F364')
 
 #auto capture payments
 Spree::Config.set(:auto_capture => true)
