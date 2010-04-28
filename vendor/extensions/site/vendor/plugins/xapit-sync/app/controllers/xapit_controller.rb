9a834d1a5f9a1cbc455ffc30def4fa54e6b7d488
Brian Quinn <briandquinn@gmail.com>
2010-04-28 15:12:47 +0100

    Removed xapit_sync plugin, doesn't help the situation in production mode

--- vendor/extensions/site/vendor/plugins/xapit-sync/app/controllers/xapit_controller.rb
+++
@@ -1,6 +0,0 @@
-class XapitController < ApplicationController
-  def reload
-    Xapit::Config.database.reopen
-    render :nothing => true
-  end
-end
--- vendor/extensions/site/vendor/plugins/xapit-sync/config/routes.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.xapit "xapit/:action", :controller => "xapit"
-end
