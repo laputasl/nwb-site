b570b784049aab8914f7c22d61f40c0bd111c469
Brian Quinn <briandquinn@gmail.com>
2010-04-29 18:28:24 +0100

    Preserve query_string on legacy product redirects

--- vendor/extensions/site/app/metal/legacy_redirect.rb
+++ vendor/extensions/site/app/metal/legacy_redirect.rb
@@ -3,14 +3,18 @@ require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Ra
 
 class LegacyRedirect
   @legacy_products = /p\d+.cfm$/
-  def self.call(env)  
+  def self.call(env)
     if pos = env["PATH_INFO"] =~ @legacy_products
       # redirect legacy products with url of the form XXXXX-pYY.cfm
-      path = env["PATH_INFO"]       
-      id = path[pos..(path.length)].gsub("p", "").gsub(".cfm", "")  
-      product = Product.find(:all, :conditions => ["legacy_id = ? AND `from` = ?", id, env['wellbeing-domain']]).first    
+      path = env["PATH_INFO"]
+      id = path[pos..(path.length)].gsub("p", "").gsub(".cfm", "")
+      product = Product.find(:all, :conditions => ["legacy_id = ? AND `from` = ?", id, env['wellbeing-domain']]).first
       if product
-        [301, {"Location" => "/products/#{product.permalink}", "Content-Type" => "text/html"}, []]
+        if env["QUERY_STRING"].blank?
+          [301, {"Location" => "/products/#{product.permalink}", "Content-Type" => "text/html"}, []]
+        else
+          [301, {"Location" => "/products/#{product.permalink}?#{env["QUERY_STRING"]}", "Content-Type" => "text/html"}, []]
+        end
       else
         [404, {"Content-Type" => "text/html"}, ["Not Found"]]
       end
