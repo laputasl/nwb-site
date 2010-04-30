fad04e5254188cd4c5cb5116ed6fe39a731c8f18
Brian Quinn <briandquinn@gmail.com>
2010-04-30 20:06:21 +0100

    Changed admin order search

--- vendor/extensions/site/site_hooks.rb
+++ vendor/extensions/site/site_hooks.rb
@@ -10,7 +10,7 @@ class SiteHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_orders_index_headers, :partial => "admin/orders/index_headers"
   insert_after :admin_orders_index_rows, :partial => "admin/orders/index_rows"
-  insert_before :admin_orders_index_search, :partial => "admin/orders/index_search_fields"
+  replace :admin_orders_index_search, :partial => "admin/orders/index_search_fields"
 
   insert_after :admin_products_index_headers, :partial => "admin/products/index_headers"
   insert_after :admin_products_index_rows, :partial => "admin/products/index_rows"
