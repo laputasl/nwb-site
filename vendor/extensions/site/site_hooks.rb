9667f8a0fe909cf998b53ea6304b91085edf1b6f
Brian Quinn <briandquinn@gmail.com>
2010-05-03 18:43:25 +0100

    Added new relic

--- vendor/extensions/site/site_hooks.rb
+++ vendor/extensions/site/site_hooks.rb
@@ -11,6 +11,7 @@ class SiteHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_orders_index_headers, :partial => "admin/orders/index_headers"
   insert_after :admin_orders_index_rows, :partial => "admin/orders/index_rows"
   replace :admin_orders_index_search, :partial => "admin/orders/index_search_fields"
+  replace :admin_users_index_search, :partial => "admin/users/search"
 
   insert_after :admin_products_index_headers, :partial => "admin/products/index_headers"
   insert_after :admin_products_index_rows, :partial => "admin/products/index_rows"
