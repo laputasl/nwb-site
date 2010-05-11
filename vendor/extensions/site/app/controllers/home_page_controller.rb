335c1101d5cb7db49e96555c963d7a89c8831e42
Brian Quinn <briandquinn@gmail.com>
2010-05-11 17:43:42 +0100

    A couple of minor performance tweaks based on New Relic reports

--- vendor/extensions/site/app/controllers/home_page_controller.rb
+++ vendor/extensions/site/app/controllers/home_page_controller.rb
@@ -17,10 +17,10 @@ class HomePageController < Spree::BaseController
     else
       #TODO: Figure out what criteria we will use to display products on homepages.
       if @site.code == "pwb"
-        @best_selling_cats = Product.find(Taxon.find([9998,3002]).map(&:product_ids).inject {|x, y| x &  y }).find_all {|p| !p.deleted? && p.available_on < Time.now}
-        @best_selling_dogs = Product.find(Taxon.find([9998,3001]).map(&:product_ids).inject {|x, y| x &  y }).find_all {|p| !p.deleted? && p.available_on < Time.now}
+        @best_selling_cats = Product.find(Taxon.find([9998,3002]).map(&:product_ids).inject {|x, y| x &  y }, :include => [:images, {:master => :volume_prices}]).find_all {|p| !p.deleted? && p.available_on < Time.now}
+        @best_selling_dogs = Product.find(Taxon.find([9998,3001]).map(&:product_ids).inject {|x, y| x &  y }, :include => [:images, {:master => :volume_prices}]).find_all {|p| !p.deleted? && p.available_on < Time.now}
       else
-        @best_selling_products = Product.active.in_taxon(9998).find_all_by_store_id(@site.id)
+        @best_selling_products = Product.active.in_taxon(9998).find_all_by_store_id(@site.id, :include => [:images, {:master => :volume_prices}])
       end
 
       render :partial => "#{@site.code}_show", :layout => true
