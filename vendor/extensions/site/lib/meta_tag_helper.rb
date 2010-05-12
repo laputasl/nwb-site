fb2e7bc81ba48bddd189372b67267be058ea897a
Brian Quinn <briandquinn@gmail.com>
2010-05-12 09:43:00 +0100

    Changed meta_skip to use env[request_path] which works with sub-uri urls, #682

--- vendor/extensions/site/lib/meta_tag_helper.rb
+++ vendor/extensions/site/lib/meta_tag_helper.rb
@@ -1,8 +1,8 @@
 module MetaTagHelper
 
-  # Renders a meta tag for use in the HEAD section of an html document to indicate that page should not be indexed 
+  # Renders a meta tag for use in the HEAD section of an html document to indicate that page should not be indexed
   def meta_skip
-    if request.path =~ /\/login|\/pets|\/people|\/orders|\/users/     
+    if request.env["REQUEST_PATH"] =~ /\/login|\/pets|\/people|\/orders|\/users/
       tag :meta, :name => "ROBOTS", :content => "NOINDEX, NOFOLLOW"
     end
   end
