6f04648d264b1953bc2b4f3fcd5fc52f679e428c
Adam van den Hoven <adamvandenhoven@naturalwellbeing.com>
2010-06-14 13:28:22 -0700

    add power reviews feed

--- vendor/extensions/site/app/controllers/feeds_controller.rb
+++ vendor/extensions/site/app/controllers/feeds_controller.rb
@@ -4,6 +4,7 @@ class FeedsController < Spree::BaseController
     feed_name = params[:feed]
     respond_to do |format|
       format.xml { render :template => "feeds/#{feed_name}", :layout => false, :locals =>{:products => Product.active}}
+      format.csv { render :template => "feeds/#{feed_name}.csv", :layout => false, :locals =>{:products => Product.active}}
     end
   end
 end
