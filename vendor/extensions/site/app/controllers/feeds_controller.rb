47fb094c9a9763b0149d7b8ea8e54da007bb84a3
Adam van den Hoven <adamvandenhoven@naturalwellbeing.com>
2010-06-14 10:39:02 -0700

    making feeds more reusable

--- vendor/extensions/site/app/controllers/feeds_controller.rb
+++ vendor/extensions/site/app/controllers/feeds_controller.rb
@@ -1,7 +1,10 @@
 class FeedsController < Spree::BaseController
   helper FeedsHelper
   def show
+    # response.headers['Content-type'] = 'text/xml; charset=utf-8'
     feed_name = params[:feed]
-    render :template => 'feeds/sli',:layout => false, :locals =>{:products => Product.active}
+    respond_to do |format|
+      format.xml { render :template => "feeds/#{feed_name}", :layout => false, :locals =>{:products => Product.active}}
+    end
   end
 end
--- vendor/extensions/site/config/routes.rb
+++ vendor/extensions/site/config/routes.rb
@@ -15,4 +15,5 @@ end
 
 map.category_taxon '/c/*path', :controller => 'taxons', :action => 'show'
 map.brand_taxon '/b/*path', :controller => 'taxons', :action => 'show'
-map.feed '/feed/:feed',  :controller => 'feeds', :action => 'show'
+map.feed '/feed/:feed.:format',  :controller => 'feeds', :action => 'show'
+# map.feed '/feed/:feed',  :controller => 'feeds', :action => 'show'
