3378e74f163635c0c5452273ccd2d5d96dfa96cc
Adam van den Hoven <adamvandenhoven@naturalwellbeing.com>
2010-06-14 10:47:55 -0700

    Revert "making feeds more reusable"
    This reverts commit 47fb094c9a9763b0149d7b8ea8e54da007bb84a3.

--- vendor/extensions/site/app/controllers/feeds_controller.rb
+++ vendor/extensions/site/app/controllers/feeds_controller.rb
@@ -1,10 +1,7 @@
 class FeedsController < Spree::BaseController
   helper FeedsHelper
   def show
-    # response.headers['Content-type'] = 'text/xml; charset=utf-8'
     feed_name = params[:feed]
-    respond_to do |format|
-      format.xml { render :template => "feeds/#{feed_name}", :layout => false, :locals =>{:products => Product.active}}
-    end
+    render :template => 'feeds/sli',:layout => false, :locals =>{:products => Product.active}
   end
 end
